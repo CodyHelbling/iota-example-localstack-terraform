1be79c90e3162064231a60e82e0f5d356044233a
Cody Helbling <cody.helbling@vacasa.com>
2019-03-15 07:59:02 -0600

    Remove confusing comment.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,4 @@
 provider "aws" {
-  //  When alias is set, it creates an additional provider configuration.
-  //  For providers that have no required configuration arguments, the implied empty configuration is considered to be
-  //  the default provider configuration.
-  //  alias = "localstack"
-
   s3_force_path_style         = true
   skip_credentials_validation = true
   skip_metadata_api_check     = true
@@ -18,11 +13,6 @@ provider "aws" {
     lambda = "http://localhost:4574"
   }
 }
-
-//provider "aws" {
-//  //  The provider block without alias set is known as the default provider configuration.
-//  region = "us-west-2"
-//}
 
 module "iota-lambda" {
   source        = "../iota-lambda/terraform"
